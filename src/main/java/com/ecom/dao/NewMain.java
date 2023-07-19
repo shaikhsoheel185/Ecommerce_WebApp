a9ddb6a76ac8af6275c50fbd74268d35cc6e25bd
shaikh soheel <shaikhsoheel185@gmail.com>
2023-07-19 14:57:41 +0530

    undo this

---
+++ src/main/java/com/ecom/dao/NewMain.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.ecom.dao;
+
+/**
+ *
+ * @author shaikh
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("com.ecom.dao.NewMain.main()");
+         System.out.println("com.ecom.dao.NewMain.main()");
+          System.out.println("com.ecom.dao.NewMain.main()");
+           System.out.println("com.ecom.dao.NewMain.main()");
+    }
+    
+}
